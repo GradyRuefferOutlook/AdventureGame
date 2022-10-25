5c8d0d76ca417f258987d7f0a63a2930d87c41bb
GradyRuefferOutlook <gradruef152@outlook.com>
2022-10-25 11:31:20 -0400

    Add project files.

---
+++ AdventureGame/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdventureGame
+{
+    public partial class Form1 : Form
+    {
+        public struct Item
+        {
+           public String itemName;
+           public bool have;
+        }
+
+        Item[] items = new Item[26];
+
+        struct Scene
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            items[0].itemName = "evil rock";
+            items[1].itemName = "bucket";
+            items[2].itemName = "holy bucket";
+            items[3].itemName = "stick";
+            items[4].itemName = "sword";
+            items[5].itemName = "axe";
+            items[6].itemName = "totem";
+            items[7].itemName = "voodoo doll";
+            items[8].itemName = "broomstick";
+            items[9].itemName = "coconut";
+            items[10].itemName = "crown of thornes";
+            items[11].itemName = "pandoras box";
+            items[12].itemName = "decapitated head";
+            items[13].itemName = "the one ring";
+            items[14].itemName = "plane";
+            items[15].itemName = "power glove";
+            items[16].itemName = "companion cube";
+            items[17].itemName = "pocket watch";
+            items[18].itemName = "plank";
+            items[19].itemName = "cloth";
+            items[20].itemName = "chain";
+            items[21].itemName = "hook";
+            items[22].itemName = "metalic hand-held item";
+            items[23].itemName = "slingshot";
+            items[24].itemName = "poison fang";
+            items[25].itemName = "torch";
+
+            for (int i = 0; i <= 25; i++)
+            {
+                items[i].have = false;
+            }
+
+        }
+    }
+}
